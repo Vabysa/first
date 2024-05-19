399c6c46e78c38ba7f064c5c34b9966a59f65ad5
Vabysa <vabysaa@gmail.com>
2024-05-19 14:09:16 +0330

    slama badi

--- print(sss).py
+++ print(sss).py
@@ -1,1 +1,13 @@
-print(sss)
+print(sss)\
+
+
+from anon_browser import AnonBrowser
+
+ab = AnonBrowser(user_agents=['User-agent', 'superSecretBrowser'])
+
+for attempt in range(1, 5):
+    ab.anonymize()
+    print('[*] Fetching page')
+    response = ab.open('http://kittenwar.com')
+    for cookie in ab.cookie_jar:
+        print(cookie)
